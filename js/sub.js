6a1b0657233065e05d86b18772a63c06275b406f
junseon1 <147118487+junseon1@users.noreply.github.com>
2023-10-10 14:57:47 +0900

    Add files via upload

---
+++ js/sub.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    const swiper = new Swiper('.main', {
+        // 3초마다 자동실행
+        autoplay:{
+            delay:3000,
+            disableOnInteraction: false,
+        },
+        // 1,2,3,1,2,3,... 무한루프 
+        loop: true,
+        // 페이지네이션(동그란 버튼)
+        pagination: {
+        el: '.main .swiper-pagination', 
+        clickable:true
+        }
+    });
+    let winTop=$(this).scrollTop();
+    // .scrollSet 영역 개수만큼 반복하는 메서드 (each())
+    $('.scrollSet').each(function(){
+        // $(this) : .scrollSet(인덱스 0~4)
+        let secTop=$(this).offset().top;
+        // section의 bottom 값
+        let secBottom=secTop+$(this).height();
+        // 만약 secTop이 winTop보다 작고 secBottom이 winTop보다 크면
+        // section의 자식객체에 active 추가
+        if(secTop < winTop && secBottom > winTop){
+        $(this).find('.ani').addClass('active');
+        }
+        
+    });
+    
+    
+   
+});
